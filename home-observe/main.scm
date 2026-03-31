59c9284bd0f7dbc39e84239a7c6f60a17a2ef8c5
Fabian Brosda <f.brosda@gmx.de>
2026-03-31 20:39:34 +0200

    continue plenticore impl

--- home-observe/main.scm
+++ home-observe/main.scm
@@ -2,22 +2,11 @@
 -L ../ -e main -s
 !#
 
-(use-modules (home-observe scram-auth)
-             (web client)
-             (web uri)
-             (json))
+(use-modules ((home-observe plenticore) #:prefix plenticore:))
+
+(define* (read-config #:optional (path "/etc/home-observe.cfg"))
+  (with-input-from-file path  read))
 
 (define (main args)
-  (with-rfc5802-auth "***"
-                     (lambda (token)
-                       (call-with-values (lambda ()
-                                           (http-post (build-uri 'http #:host "plenticore.fritz.box" #:path "/api/v2/events/latest")
-                                                      #:headers `((content-type application/json (charset . "utf-8"))
-                                                                  (accept (application/json))
-                                                                  (accept-encoding (1000 . "gzip") (1000 . "deflate"))
-                                                                  (authorization . (bearer ,token)))
-                                                      #:body (scm->json-string `(("language" . "de-de")))))
-                         (lambda (res body)
-                           (format #t "~s~%" res)
-                           (format #t "~s~%" (json-string->scm (ensure-string body)))))))
-  (newline))
+  (let ((cfg (read-config "./home-observe.cfg")))
+    (plenticore:observe (assoc-ref cfg "plenticore"))))
